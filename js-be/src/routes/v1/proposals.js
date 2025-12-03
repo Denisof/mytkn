47d2e1ff9bf53ceb185321e78fa95c61cca1bc83
denysloshchakov <dloschakov@gmail.com>
2025-12-03 01:33:22 +0100

    JS-DE removed

--- js-be/src/routes/v1/proposals.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const proposalController = require('../../controllers/proposalController');
-
-router.get('/', proposalController.getAll);
-router.get('/:proposalId', proposalController.getById);
-
-module.exports = router;
